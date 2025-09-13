46ba2b9c0b59be239777829054ec9f73f459e26c
amine <amine.m.a1998@gmail.com>
2025-09-13 13:54:42 +0100

    add InMemoryUserDetails to SecurityFilterChain

--- src/main/java/com/example/graph/security/SecurityFilter.java
+++ src/main/java/com/example/graph/security/SecurityFilter.java
@@ -4,6 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,10 +16,26 @@ public class SecurityFilter {
         http
                 .csrf(csrf -> csrf.disable()) // Disable CSRF for API
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/**").authenticated() // Protect /graphql
+                        .requestMatchers("/api/**").hasRole("ADMIN") // Protect /graphql
                         .anyRequest().permitAll()  // Everything else is public
                 )
                 .httpBasic(Customizer.withDefaults()); // Use basic auth (username/password)
         return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails admin = User.withDefaultPasswordEncoder() // for demo only
+                .username("admin")
+                .password("password")
+                .roles("ADMIN")
+                .build();
+        UserDetails user = User.withDefaultPasswordEncoder() // for demo only
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user,admin);
     }
 }
